BIPES/BIPES

commit 840047610470d6ab7b7bfb3a396a61d65130742f

Enable split screen by right clicking tab name

ui/core/code.js
@@ -174,103 +174,214 @@ Code.LANG = Code.getLang();
  */
 
 Code.TABS_ = ['blocks', 'console', 'files', 'device', 'programs', 'databoard', 'mqtt', 'iot'];
-//Code.TABS_ = ['blocks', 'console', 'javascript', 'python', 'xml'];
 
-Code.selected = 'blocks';
+Code.current = ["blocks", "",""]
 
 /**
- * Switch the visible pane when a tab is clicked.
- * @param {string} clickedName Name of tab clicked.
+ * Switch the visible pane when a tab is clicked, allows splitting screen.
+ * @param {string} navigation Name of tab clicked.
+ * @param {Number} _pos Position, 0 to full, 1 to full/left and 2 to right.
  */
-Code.tabClick = function(clickedName) {
+Code.handleLink = (_navigation, _pos) => {
+  let _pos0 = _pos == 2 ? 1 : 2
+  let crt = Code.current
 
-  Code.auto_mode = false;
-  // Deselect all tabs and hide all panes.
-  for (var i = 0; i < Code.TABS_.length; i++) if (Code.TABS_ [i] != clickedName){
-    var name = Code.TABS_[i];
-    document.getElementById('tab_' + name).className = 'taboff';
-    document.getElementById('content_' + name).className = 'content off';
+  let turnOff = (elem, pos) => {
+    let _tab  = get(`#content_${elem}`),
+        _nav  = get(`#tab_${elem}`)
+    Code.deinitContent(elem)
+	  _nav.classList.remove('on')
+	  // just to avoid animation glitch
+	  if (pos == 1)
+    	_tab.classList.add(`_pos1`)
+    if (pos == 2)
+    	_tab.classList.add(`_pos2`)
+  	_tab.classList.remove(`pos${pos}`)
+	  Animate.off(_tab)
   }
 
-  // Select the active tab.
-  Code.selected = clickedName;
-  document.getElementById('tab_' + clickedName).className = 'tabon';
-  // Show the selected pane.
-  document.getElementById('content_' + clickedName).className = 'content';
-  Code.renderContent();
-  if (clickedName == 'blocks') {
-    Code.workspace.setVisible(true);
-    Code.auto_mode = true;
-  } else {
+  let turnOn = (elem, pos) => {
+    let _tab  = get(`#content_${elem}`),
+        _nav  = get(`#tab_${elem}`)
+    Code.renderContent(elem)
+
+
+    _nav.classList.add('on')
+	  Animate.on(_tab)
+	  _tab.classList.remove("_pos1", "_pos2")
+	  if (pos != 0)
+  	  _tab.classList.add(`pos${pos}`)
+  }
+
+  // Interpreting link
+  if (_pos == 0){
+    turnOn(_navigation, 0)
+    Code.current = [_navigation, '', '']
+    return
+  }
+
+  // User switch opened section
+  if (crt[_pos0] == _navigation) {
+    let _tab  = get(`#content_${crt[_pos]}`),
+        _tab0 = get(`#content_${crt[_pos0]}`)
+
+ 	  _tab.classList.remove(`pos${_pos}`)
+ 	  _tab.classList.add(`pos${_pos0}`)
+ 	  _tab0.classList.remove(`pos${_pos0}`)
+ 	  _tab0.classList.add(`pos${_pos}`)
+ 	  Code.current = ['', crt[2], crt[1]]
+ 	  return
+  }
+
+  // User click in the same occupying the whole screen
+  if (crt [0] == _navigation && crt [1] == '' && crt [2] == '')
+    return
+
+  // User left click in a new section while another is occuppying the whole screen
+  if (_pos == 1 && crt[0] != ''){
+    turnOff(crt[0], 0)
+    crt[0] = _navigation
+    turnOn(crt[0], 0)
+    Code.resizeContent()
+    return
+  }
+
+  // Both have sections, user left click in one again -> occupy all screen
+  if (_pos == 1){
+    if (crt [_pos] == _navigation && crt[_pos0] != ''){
+      turnOff(crt[_pos0], _pos0)
+      let _tab  = get(`#content_${crt[_pos]}`)
+   	  _tab.classList.remove(`pos${_pos}`)
+      Code.current = [_navigation, '', '']
+      Code.resizeContent()
+      return
+    }
+  }
+
+  // Both have sections, user right click in one again -> deinit
+  if (_pos == 2){
+    if (crt [_pos] == _navigation && crt[_pos0] != ''){
+      turnOff(crt[_pos], _pos)
+      let _tab0  = get(`#content_${crt[_pos0]}`)
+   	  _tab0.classList.remove(`pos${_pos0}`)
+      Code.current = [crt[_pos0], '', '']
+      Code.resizeContent()
+      return
+    }
+  }
+
+  // User click in a new section to ocuppy
+  if (crt[_pos] != _navigation && crt[0] == ''){
+    if (crt[_pos] != '')
+      turnOff(crt[_pos], _pos)
+    turnOn(_navigation, _pos)
+    Code.current[_pos] = _navigation
+    Code.resizeContent()
+    return
+  }
+
+  // User right click in a new section while another is occupying everthing
+  if (_pos == 2 && crt[_pos] != _navigation && crt[0] != ''){
+    let _tab  = get(`#content_${crt[0]}`)
+ 	  _tab.classList.add(`pos1`)
+    turnOn(_navigation, 2)
+    Code.current = ['', crt[0], _navigation]
+    Code.resizeContent()
+    return
+  }
+}
+
+/**
+ * Handle the rendering of the tabs;
+ * @param {string} _navigation Name of tab.
+ */
+Code.renderContent = (_navigation) => {
+  let content = get(`#content_${_navigation}`)
+  switch (_navigation) {
+    case  "databoard":
+      if (!window.frames[3].inited) {
+        if (typeof window.frames[3].modules == 'object' && typeof window.frames[3].modules.Workspaces == 'object') {
+          window.frames[3].initDataStorage()
+        } else {
+          /** wait to databoad to load */
+          var interval = setInterval(() => {
+            if (typeof window.frames[3].modules == 'object' && typeof window.frames[3].modules.Workspaces == 'object') {
+              window.frames[3].initDataStorage()
+              if (window.frames[3].inited)
+                clearInterval(interval)
+            }
+          }, 500);
+        }
+      } else
+      window.frames[3].initGrid()
+      break
+    case "blocks":
+      Code.workspace.setVisible(true)
+      Code.auto_mode = true
+      Blockly.svgResize(Code.workspace)
+      break
+    case "files":
+      if (Files.editor.init == undefined) {
+        //horrible fix for line numbers not showing.
+        Files.editor.setValue(new Array(9).fill('\r\n').join(''))
+        Files.editor.setValue('')
+        Files.editor.init = true
+      }
+      Files.handleCurrentProject()
+      break
+    case "console":
+      term.resize()
+      break
+    case "device":
+    case "programs":
+    case "iot":
+    case "mqtt":
+      break
+  }
+  content.focus()
+};
+/**
+ * Handle the resizing of the tabs on split mode;
+ * @param {string} _navigation Name of tab.
+ */
+Code.resizeContent = (_navigation) => {
+  Code.current.forEach (key => {
+    switch (key) {
+      case "blocks":
+        setTimeout(()=>{Blockly.svgResize(Code.workspace)}, 250)
+        break
+      case "files":
+        Files.resize()
+        break
+      case "console":
+        term.resize()
+        break
+      case "databoard":
+      case "device":
+      case "programs":
+      case "iot":
+      case "mqtt":
+        break
+    }
+  })
+};
+
+/**
+ * Deinit a tab (if it supports).
+ * @param {string} _navigation Name of tab to deinit.
+ */
+Code.deinitContent = (_navigation) => {
+  switch (_navigation) {
+  case  "databoard":
+    if(window.frames[3].grid_inited)
+      window.frames[3].deinitGrid()
+    break
+  case "blocks":
     Code.workspace.setVisible(false);
     Code.auto_mode = false;
+    break
   }
-  Blockly.svgResize(Code.workspace);
-
-};
-
-/**
- * Populate the currently selected pane with content generated from the blocks.
- */
-Code.renderContent = function() {
-  var content = document.getElementById('content_' + Code.selected);
-  if (content.id == 'content_files') {
-    if (Files.editor.init == undefined) {
-      //horrible fix for line numbers not showing.
-      Files.editor.setValue(new Array(9).fill('\r\n').join(''))
-      Files.editor.setValue('')
-      Files.editor.init = true;
-    }
-    Files.handleCurrentProject();
-    var nArea = document.getElementById('content_files');
-    nArea.focus();
-  } else if (content.id == 'content_device') {
-    var nArea = document.getElementById('content_device');
-    nArea.focus();
-  } else if (content.id == 'content_databoard') {
-    var nArea = document.getElementById('content_databoard');
-    if (!window.frames[3].inited) {
-      if (typeof window.frames[3].modules == 'object' && typeof window.frames[3].modules.Workspaces == 'object') {
-        window.frames[3].initDataStorage()
-      } else {
-        /** wait to databoad to load */
-        var interval = setInterval(() => {
-          if (typeof window.frames[3].modules == 'object' && typeof window.frames[3].modules.Workspaces == 'object') {
-            window.frames[3].initDataStorage()
-            if (window.frames[3].inited)
-              clearInterval(interval);
-          }
-        }, 500);
-      }
-    } else
-      window.frames[3].initGrid();
-    nArea.focus();
-  } else if (content.id == 'content_programs') {
-    var nArea = document.getElementById('content_programs');
-    nArea.focus();
-  } else if (content.id == 'content_console') {
-    var xmlTextarea = document.getElementById('content_console');
-    xmlTextarea.focus();
-  /*STARTDEPRECATED*/
-  } else if (content.id == 'content_iot') {
-    var nArea = document.getElementById('content_iot');
-    nArea.focus();
-  } else if (content.id == 'content_mqtt') {
-    var nArea = document.getElementById('content_mqtt');
-    nArea.focus();
-  }
-  /*ENDDEPRECATED*/
-
-  if(content.id != 'content_databoard')
-    if(window.frames[3].grid_inited)
-      window.frames[3].deinitGrid();
-
-
-  // } else if (content.id == 'content_javascript') {
-  //   Code.generateCode(Blockly.JavaScript);
-  //   Code.toDOM(Code.javascriptCode, 'py', 'content_javascript');
-  // }
-};
+}
 
 /**
  * Attempt to generate the code and display it in the UI, pretty printed.
@@ -438,10 +549,6 @@ Code.init = function() {
             wheel: true}
       });
 
-  // Add to reserved word list: Local variables in execution environment (runJS)
-  // and the infinite loop detection function.
-  Blockly.JavaScript.addReservedWords('code,timeouts,checkTimeout');
-
   Code.loadBlocks('');
 
   if ('BlocklyStorage' in window) {
@@ -449,7 +556,7 @@ Code.init = function() {
     BlocklyStorage.backupOnUnload(Code.workspace);
   }
 
-  Code.tabClick(Code.selected);
+  Code.handleLink(Code.current[0], 0);
 
   //Code.bindClick('trashButton',
   //    function() {Code.discard(); Code.renderContent();});
@@ -474,14 +581,22 @@ Code.init = function() {
     linkButton.className = 'disabled';
   }
 
+  // Bind left click/tap and right click on tabs
   for (var i = 0; i < Code.TABS_.length; i++) {
-    var name = Code.TABS_[i];
-    Code.bindClick('tab_' + name,
-        function(name_) {return function() {Code.tabClick(name_);};}(name));
+    let name = Code.TABS_[i];
+    let tab = get(`#tab_${name}`)
+    tab.addEventListener("click", (ev) => {
+      ev.preventDefault()
+      Code.handleLink(name, 1)
+    })
+    tab.addEventListener("contextmenu", (ev) => {
+      ev.preventDefault()
+      Code.handleLink(name, 2)
+    })
   }
   Blockly.svgResize(Code.workspace);
 
-    Code.workspace.registerButtonCallback('installPyLib', function(button) {
+  Code.workspace.registerButtonCallback('installPyLib', function(button) {
 
 	var lib = button.text_.split(" ")[1];
 	console.log(button.text_);

ui/core/ui.js
@@ -394,6 +394,7 @@ class responsive {
     window.onresize = () => {
       Files.resize ();
       term.resize ();
+
       this.mobile = window.innerWidth < 60*$em ? true : false;
     };
   }
@@ -625,8 +626,6 @@ workspace.prototype.change = function () {
        if (blocks.length != 0) UI ['notify'].send (MSG['wrongDevicePin']);
     }
 
-    Code.renderContent (); // renders selected tab
-
     Channel ['webserial'].packetSize = parseInt(selected.serial_packet_size);
     Channel ['webserial'].speed = parseInt(selected.speed);
 
@@ -820,16 +819,16 @@ workspace.prototype.loadDataboard = function (JSON_) {
     let databoard = JSON.parse(JSON_)
     /** Test if iframe is a freeboard */
     if (/\/databoard/.test(window.frames[3].location.pathname)) {
-      if (typeof  window.frames[3].modules == 'object') {
+      if (typeof window.frames[3].modules == 'object') {
         window.frames[3].modules.Workspaces.deinit()
         window.frames[3].modules.Workspaces.clearLocalStorage();
         window.frames[3].modules.Workspaces.uncompress(databoard);
-        if (get('#tab_databoard').className=="tabon")
+        if (get('#tab_databoard').classList.contains("on"))
           window.frames[3].modules.Workspaces.init()
       } else {
-        /** wait to freeboard iframe to load */
+        /** wait to databoard iframe to load */
         var interval = setInterval(() => {
-          if (typeof  window.frames[1].freeboard == 'object') {
+          if (typeof window.frames[3].mopdules == 'object') {
             window.frames[3].modules.Workspaces.deinit()
             window.frames[3].modules.Workspaces.clearLocalStorage();
             window.frames[3].modules.Workspaces.uncompress(databoard);
@@ -838,7 +837,7 @@ workspace.prototype.loadDataboard = function (JSON_) {
         }, 500);
       }
     } else
-      UI ['notify'].log('iFrame is not a freeboard');
+      UI ['notify'].log('iFrame is not a Databoard');
   } catch (e) {
     UI ['notify'].log(e);
   }

ui/core/utils.js
@@ -313,7 +313,12 @@ class files {
    * Resize the ``codemirror`` editor, triggered by ``window.onresize`` event.
    */
   resize () {
-    this.editor.setSize(window.innerWidth - (18*$em),window.innerHeight - (6*$em))
+    if (!Code.current.includes('files'))
+      return
+    if (Code.current[0] == 'files')
+      this.editor.setSize(window.innerWidth - (18*$em),window.innerHeight - (6*$em))
+    else
+      this.editor.setSize((window.innerWidth/2) - (18*$em),window.innerHeight - (6*$em))
   }
   /**
    * Upload file to device.
@@ -725,6 +730,24 @@ class DOM {
   }
 }
 
+
+/** Enables basic css3 animations in the DOM Node element*/
+class Animate {
+  constructor (){}
+  static off (dom, callback){
+    dom.classList.remove('on')
+    setTimeout(()=>{
+      dom.classList.remove('ani', 'on')
+      if (callback != undefined)
+        callback ()
+      }, 250)
+  }
+  static on (dom){
+    dom.classList.add('ani')
+    setTimeout(()=>{dom.classList.add('ani', 'on')}, 250)
+  }
+}
+
 /** Handle ``xterm.js`` terminal*/
 class term {
   constructor () {
@@ -767,8 +790,17 @@ class term {
    * Resize the ``xterm.js`` terminal, triggered by ``window.onresize`` event.
    */
   static resize () {
-    let cols = Math.max(50, Math.min(200, (window.innerWidth - 4*$em) / 7)) | 0;
-    let rows = Math.max(15, Math.min(40, (window.innerHeight - 20*$em) / 12)) | 0;
+    if(!Code.current.includes('console'))
+      return
+
+    let cols
+    if (Code.current[0] == 'console')
+      cols = Math.max(50, Math.min(200, (window.innerWidth - 4*$em) / 7)) | 0
+    else
+      cols = Math.max(50, Math.min(200, ((window.innerWidth)/2 - 4*$em) / 7)) | 0
+
+    let rows = Math.max(15, Math.min(40, (window.innerHeight - 20*$em) / 12)) | 0
+
     terminal.resize(cols, rows);
   }
 }

ui/index.html
@@ -194,7 +194,7 @@ document.body.appendChild(el);
   </div>
 
   <div id="content_blocks" class="content"></div>
-  <div id="content_console" class="content off">
+  <div id="content_console" class="content">
        <span class="icon" id="tipsGeneric"><tt>Ctrl+Insert</tt> to copy and <tt>Shift+Insert</tt> to paste</span>
     <tr height="25%" id="termTR">
      <td>
@@ -245,7 +245,7 @@ document.body.appendChild(el);
 
 <!-- ################################################## -->
 
-  <div id="content_programs" class="content off">
+  <div id="content_programs" class="content">
     <center>
 	    <!--
     <iframe src="https://bipes-271213.appspot.com/list" width="100%" height="500" frameBorder="0"></iframe>
@@ -257,7 +257,7 @@ document.body.appendChild(el);
 
 <!-- ################################################## -->
 
-  <div id="content_device" class="content off">
+  <div id="content_device" class="content">
     <center>
       <label id="device_title"><b>ESP8266</b><br></label>
     <br>
@@ -275,7 +275,7 @@ document.body.appendChild(el);
 
 <!-- ################################################## -->
 
-  <div id="content_files" class="content off">
+  <div id="content_files" class="content">
 
 
   <div id="file-boxes">
@@ -336,19 +336,19 @@ document.body.appendChild(el);
 
 <!-- ################################################## -->
 
-  <div id="content_iot" class="content off">
+  <div id="content_iot" class="content">
      <iframe id="content_iot" src="freeboard/index.html" width="100%" height="100%" frameBorder="0"></iframe>
   </div>
 
 <!-- ################################################## -->
 
-<div id="content_mqtt" class="content off" style="padding: 0;">
+<div id="content_mqtt" class="content" style="padding: 0;">
   <iframe id="easymqtt_iframe" src="../easymqtt/index.html" width="100%" height="100%" frameBorder="0"></iframe>
 </div>
 
 <!-- ################################################## -->
 
-<div id="content_databoard" class="content off" style="padding: 0;">
+<div id="content_databoard" class="content" style="padding: 0;">
   <iframe id="databoard_iframe" src="../databoard/index.html" width="100%" height="100%" frameBorder="0"></iframe>
 </div>
 
@@ -397,7 +397,6 @@ term.init('#term') //init expanded class;
 
 //global Utilities variable and functions (see core/utils.js)
 var Files = new files ('#fileList');
-Files.resize();
 
 // global User Interface variable and functions (see core/ui.js) */
 var UI = {};

ui/style.css
@@ -32,10 +32,6 @@ tt {
   text-align: right;
 }
 
-html[dir="RTL"] .farSide {
-  text-align: left;
-}
-
 /* Buttons */
 button.icon, label.button.icon {
   border-radius: 2rem;
@@ -88,6 +84,7 @@ h1 {
   user-select: none;
   width: 100vw;
   box-sizing: border-box;
+  border-bottom: 1px solid #ccc;
 }
 .top-menu > .logo {
   display: flex;
@@ -318,38 +315,23 @@ input.checkswitch[type="checkbox"] {
     margin: 0rem 1px;
   }
 }
-.tabs > div.tabon {
+.tabs > div.on {
   box-shadow: 0 .125em .5em #ccc;
   white-space: nowrap;
   background: #3298dc;
   color: #fff;
   border-color: #3298dc;
 }
-.tabs > div.taboff {
+.tabs > div {
   cursor: pointer;
 }
-.tabs > div.taboff:hover {
+.tabs > div:hover {
   border-color: #3298dc;
   color: #3298dc;
 }
-.tabs > div.tabmin {
-  border-top-style: none !important;
-  border-left-style: none !important;
-  border-right-style: none !important;
+.tabs > div.on:hover {
+  color: #fff !important;
 }
-.tabs > div.tabmax {
-  border-top-style: none !important;
-  border-left-style: none !important;
-  border-right-style: none !important;
-  width: 99%;
-  padding-left: 10px;
-  padding-right: 10px;
-  text-align: right;
-}
-html[dir=rtl] td.tabmax {
-  text-align: left;
-}
-
 table {
   border-collapse: collapse;
   margin: 0;
@@ -360,16 +342,57 @@ td {
   padding: 0;
   vertical-align: top;
 }
-#content_blocks.off, #content_iot.off {
+#content_blocks, #content_iot {
   visibility: hidden;
 }
-.content.off:not(#content_blocks):not(#content_iot){
-  display: none !important;
+.content:not(#content_blocks):not(#content_iot){
+  display: none;
 }
-.content{
+.content {
+  width: 100vw;
+  top: 3.5rem;
   margin: 0;
   position: absolute;
-  direction: ltr;
+	opacity: 0;
+}
+.content:not(#content_blocks) {
+	transition: .125s ease left, .125s ease opacity, .125s ease width;
+}
+.content#content_blocks {
+  margin-top: -1px;
+	transition: .125s ease left, .125s ease opacity;
+}
+.content.ani#content_blocks, .content.ani#content_iot {
+  visibility: visible;
+}
+.content.ani:not(#content_console):not(#content_files) {
+  display: block !important;
+}
+.content.ani#content_console {
+  display: flex !important;
+}
+.content.ani#content_files {
+  display: grid !important;
+}
+.content.ani.on {
+  left: 0;
+  opacity: 1;
+}
+.content.pos1 {
+  width: 50vw !important;
+}
+.content._pos1 {
+  width: 50vw;
+  left: -1em !important;
+}
+.content.pos2 {
+  width: 50vw;
+  left: 50vw !important;
+  border-left: 1px solid #ccc;
+}
+.content._pos2 {
+  width: 50vw;
+  left: calc(50vw + 1em) !important;
 }
 pre.content {
   border: 1px solid #ccc;
@@ -379,7 +402,6 @@ pre.content {
   padding: 0;
 }
 #content_blocks, #content_device, #content_code, #content_console, #content_files, #content_device, #content_programs, #content_mqtt, #content_iot, #content_databoard {
-  width: 100vw;
   height: calc(100vh - 3.5rem);
 }
 #content_device, #content_code, #content_console {
@@ -413,7 +435,6 @@ pre.content {
   width: 100%
 }
 #content_console {
-  display: flex;
   flex-direction: column;
   align-items: center;
 }
@@ -461,8 +482,7 @@ pre.content {
 
 /* Files */
 #content_files {
-  display: grid;
-  grid-template-columns: 18rem calc(100vw - 18rem);
+  grid-template-columns: 18rem calc(100% - 18rem);
 }
 #file-boxes #file-header, #file-boxes #file-status  {
   padding: 0 .25em 0 .5em;
@@ -474,7 +494,6 @@ pre.content {
   align-items: center;
   justify-content: space-between;
   font-weight: bold;
-  border-top: 1px solid #ccc;
   border-bottom: 1px solid #ccc;
   border-right: 1px solid #ccc;
 }
@@ -483,7 +502,6 @@ pre.content {
 }
 #file_save_as {
   height: 2.5rem;
-  border-top: 1px solid #ccc;
   border-bottom: 1px solid #ccc;
   display: flex;
   align-items: center;

Goblincomet/BIPES_blockly

commit 04cb578fd0ff361594e19293eb6f6a98ee9d54ed

Fix Codemirror and Databoard not loading properly on firefox
Fixes issue caused by commit 840047610470d6ab7b7bfb3a396a61d65130742f

ui/core/code.js
@@ -301,21 +301,24 @@ Code.renderContent = (_navigation) => {
   let content = get(`#content_${_navigation}`)
   switch (_navigation) {
     case  "databoard":
-      if (!window.frames[3].inited) {
-        if (typeof window.frames[3].modules == 'object' && typeof window.frames[3].modules.Workspaces == 'object') {
-          window.frames[3].initDataStorage()
-        } else {
-          /** wait to databoad to load */
-          var interval = setInterval(() => {
-            if (typeof window.frames[3].modules == 'object' && typeof window.frames[3].modules.Workspaces == 'object') {
-              window.frames[3].initDataStorage()
-              if (window.frames[3].inited)
-                clearInterval(interval)
-            }
-          }, 500);
-        }
-      } else
-      window.frames[3].initGrid()
+      // Wait 10ms because the canvas of chart.js cannot be inited while not displaying
+      setTimeout(() => {
+        if (!window.frames[3].inited) {
+          if (typeof window.frames[3].modules == 'object' && typeof window.frames[3].modules.Workspaces == 'object') {
+            window.frames[3].initDataStorage()
+          } else {
+            /** wait to databoad to load */
+            var interval = setInterval(() => {
+              if (typeof window.frames[3].modules == 'object' && typeof window.frames[3].modules.Workspaces == 'object') {
+                window.frames[3].initDataStorage()
+                if (window.frames[3].inited)
+                  clearInterval(interval)
+              }
+            }, 500)
+          }
+        } else
+        window.frames[3].initGrid()
+      }, 10)
       break
     case "blocks":
       Code.workspace.setVisible(true)
@@ -324,10 +327,13 @@ Code.renderContent = (_navigation) => {
       break
     case "files":
       if (Files.editor.init == undefined) {
-        //horrible fix for line numbers not showing.
+        // Horrible fix for line numbers not showing when initing
+        // with less than ten lines
         Files.editor.setValue(new Array(9).fill('\r\n').join(''))
-        Files.editor.setValue('')
-        Files.editor.init = true
+        setTimeout(() => {
+          Files.editor.setValue('')
+          Files.editor.init = true
+        }, 10)
       }
       Files.handleCurrentProject()
       break